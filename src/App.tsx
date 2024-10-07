333e246a5673573d9bac84f08310b6c133ffdd5a
pangidoannsh <pangidoanhrp13@gmail.com>
2024-10-07 22:35:03 +0700

    feat: antd & react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
 import './App.css'
+import { lazy } from 'react';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const HomePage = lazy(() => import('./pages/HomePage'));
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+  }
+]);
 
 function App() {
   return (
     <>
-
+      <RouterProvider router={router} />
     </>
   )
 }
