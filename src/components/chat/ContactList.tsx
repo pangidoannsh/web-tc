db4da5070388c6fddfd0a15a9b9da4f894e2e4dd
pangidoannsh <pangidoanhrp13@gmail.com>
2024-12-17 14:51:51 +0700

    update contact list

--- src/components/chat/ContactList.tsx
+++ src/components/chat/ContactList.tsx
@@ -7,7 +7,7 @@ interface Props {
     onClickItem?: (contact: UserType) => void
 }
 const ContactList: FC<Props> = ({ data, onClickItem }) => (
-    <div className='flex flex-col mt-3'>
+    <div className='flex flex-col mt-3 overflow-auto v-scroll'>
         {data.map(contact => (
             <div key={contact.id} onClick={() => onClickItem?.(contact)} className={`px-2 py-3 border-t border-slate-200 flex gap-3 items-center w-full cursor-pointer hover:bg-sky-50`}>
                 <Avatar name={contact.name} imageUrl={contact.avatar} />
